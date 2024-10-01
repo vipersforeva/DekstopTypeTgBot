d444e78d9fe134b5d1a41fd8e8adf228aa275a2a
surfshtt <fk8ghtt@gmail.com>
2024-10-02 01:34:23 +0300

    1.2
    Создал логику команды /ask, а так же класс для сохранения в текстовый файл (в будущем изменю на БД)

--- src/main/java/org/example/Bot.java
+++ src/main/java/org/example/Bot.java
@@ -35,17 +35,33 @@ public class Bot extends TelegramLongPollingBot {
         String userName = user.getUserName();
         String message = msg.getText();
 
+        System.out.println(userName + " написал: " + message + "\nАйди чата: " + chatId);
+
         if(!users.containsKey(userName) || message.equals("/start")) {
             users.put(userName, new UserInfo());
             String botMsg = "*Привет!*\n" +
                     "Я телеграм бот клавиатурного тренажера *DesktopType*⌨\uFE0F\n" +
-                    "Чтобы узнать что я умею напиши /info";
+                    "Хочешь узнать что я умею? Напиши /info";
             sendFormattedMessage(chatId,botMsg);
             return;
         }
 
+        switch (users.get(userName).getQuest()){
+            case 10:
+                FileLogic.saveToFile(message + " \nChatId: " + chatId + " UserName: " + userName);
+                sendFormattedMessage(chatId,"*Спасибо!*\nВаше обращение отправлено на рассмотрение❤\uFE0F");
+                return;
+        }
+
         switch (message){
             case "/info":
+                String botMsg = "_Пока что ничего(_";
+                sendFormattedMessage(chatId,botMsg);
+                break;
+
+            case "/ask":            //question Code 10
+                sendMessage(chatId,"Расскажите о вашем предложении");
+                users.get(userName).setQuest(10);
                 break;
 
             case "/test":
@@ -55,8 +71,6 @@ public class Bot extends TelegramLongPollingBot {
             default:
                 sendMessage(chatId,"Извини, я тебя не понял!\uD83D\uDE1E");
         }
-
-        System.out.println(userName + " написал: " + message + "\nАйди чата: " + chatId);
     }
 
     public void sendMessage(String chatId, String msg){
